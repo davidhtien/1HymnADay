ca4adb721acd8f90a58045afa57e5a6d342d8b8b
David Tien <david@DHTien-ThinkPad-X220.(none)>
2014-03-14 03:03:24 -0700

    changed app.listen

--- server.js
+++ server.js
@@ -5,15 +5,14 @@ request = require('request');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
-app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
-});
 
 app.get('/', function(req, res){
     res.send('Hello world.');
 });
-app.listen(3000);
+
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
 
 var T = new Twit({
     consumer_key:         'H10n1pK0srAmOLpymYn0rg'
