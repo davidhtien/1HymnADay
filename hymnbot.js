de4b6005f3b6fcb641f55b292d01379fe8c24339
David Tien <david@DHTien-ThinkPad-X220.(none)>
2014-03-14 01:38:31 -0700

    slight changes to express require and also to open shift config

--- hymnbot.js
+++ hymnbot.js
@@ -1,12 +1,13 @@
 var Twit = require('twit');
+var express = require("express");
+var app = express();
 request = require('request');
-var app = require('express').createServer();
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
 });
 
 app.get('/', function(req, res){
