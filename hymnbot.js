0dd231e3de90a3f038e469d82c0a3f8a84764e20
David Tien <david@DHTien-ThinkPad-X220.(none)>
2014-03-14 01:32:20 -0700

    added open shift configuration lines

--- hymnbot.js
+++ hymnbot.js
@@ -2,6 +2,13 @@ var Twit = require('twit');
 request = require('request');
 var app = require('express').createServer();
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
+
 app.get('/', function(req, res){
     res.send('Hello world.');
 });
