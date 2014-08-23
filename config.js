eaeb1d9f667fbd5aad2156fac0cf89c16764e1d3
David Tien <david@DHTien-ThinkPad-X220.(none)>
2014-08-23 02:59:50 -0700

    trying to switch to heroku scheduler, added task script and config js file

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.consumer_key         = 'H10n1pK0srAmOLpymYn0rg';
+config.consumer_secret      = 'fPzrJJgnZRtUcojRhULVVerGcLaNPyKOXj9q15VoHTU';
+config.access_token         = '2357203652-ome6EWChcTqBB8LdTITRXolhaWKybmmQnHlwxaV';
+config.access_token_secret  = 'AHFAD68NZYTajEF3eaozlP2D5F7GL8iroSpiRnIX1nbJs';
+
+module.exports = config;
